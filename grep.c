9e5177017bf53c197d5b904668a4686669c90339
fatemetmhr <fatemetmhr@gmail.com>
2024-02-06 12:35:00 +0330

    bugs in wildcard check (grep) fixed

--- grep.c
+++ grep.c
@@ -6,14 +6,25 @@ int grep(const char *file, const char *word, bool print_line){
     FILE *f = fopen(file, "r");
     if(f == NULL)
         return 1;
-    char s[MAX_LINE_SIZE], buf[MAX_LINE_SIZE];
+    char s[MAX_LINE_SIZE], buf[MAX_LINE_SIZE], keep[MAX_LINE_SIZE];
     char *sep = " \n\t";
     int line = 1;
     while(fgets(s, sizeof(s), f)){
+        strcpy(keep, s);
+        int x = 0;
+        char *very_last = s + strlen(s);
         char *ptr = strtok(s, sep);
         bool re = false;
         buf[0] = '\0';
+        char *last = s;
         while(ptr != NULL){
+            while(last != ptr){
+                int len = strlen(buf);
+                buf[len] = keep[x];
+                buf[len + 1] = '\0';
+                last++;
+                x++;
+            }
             if(wildcard_match(ptr, word)){
                 strcpy(buf + strlen(buf), "\033[1;32m");
                 strcpy(buf + strlen(buf), ptr);
@@ -22,15 +33,23 @@ int grep(const char *file, const char *word, bool print_line){
             }
             else
                 strcpy(buf + strlen(buf), ptr);
-            int len = strlen(buf);
-            buf[len] = ' ';
-            buf[len + 1] = '\0';
+            while(last != ptr + strlen(ptr)){
+                last++;
+                x++;
+            }
             ptr = strtok(NULL, sep);
+        }
+        while(last != very_last){
+            int len = strlen(buf);
+            buf[len] = keep[x];
+            buf[len + 1] = '\0';
+            last++;
+            x++;
         }
         if(re){
             if(print_line)
                 printf("%d. ", line);
-            printf("%s\n", buf);
+            printf("%s", buf);
         }
         line++;
     }
